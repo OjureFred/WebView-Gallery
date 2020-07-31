f729436ab274eeda53e8e01f95ec1d0b3190a619
Fred Ojure <fredojure@hotmail.com>
2020-07-31 18:19:15 +0300

    Created foreign key and many to many key in gallery model

--- gallery/models.py
+++ gallery/models.py
@@ -4,13 +4,16 @@ from django.db import models
 class Gallery(models.Model):
     image_name = models.CharField(max_length=50)
     image_description = models.CharField(max_length=250)
+    image_location = models.ForeignKey(Location)
+    categories = models.ManyToManyField(categories)
+    save_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.image_name
     class Meta:
         ordering = ['image_name']
 
-class Category(models.Model):
+class categories(models.Model):
     category_name = models.CharField(max_length=50)
 
 class Location(models.Model):
