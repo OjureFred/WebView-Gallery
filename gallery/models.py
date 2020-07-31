0e45d81f11f2ba396c12d67b359fd9ac90545983
Fred Ojure <fredojure@hotmail.com>
2020-07-31 18:37:39 +0300

    Reorganized models.py

--- gallery/models.py
+++ gallery/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class categories(models.Model):
+    category_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category_name
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.location_name
+
 class Gallery(models.Model):
     image_name = models.CharField(max_length=50)
     image_description = models.TextField()
@@ -13,15 +25,3 @@ class Gallery(models.Model):
         return self.image_name
     class Meta:
         ordering = ['image_name']
-
-class categories(models.Model):
-    category_name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.category_name
-
-class Location(models.Model):
-    location_name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.location_name
