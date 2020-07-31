92ca785ca32c613b2ba705c16486ca7d51bda3e6
Fred Ojure <fredojure@hotmail.com>
2020-07-31 18:11:02 +0300

    Added category and location models

--- gallery/models.py
+++ gallery/models.py
@@ -2,4 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Gallery(models.Model):
-    image = model.ImaageField(upload_to='images')
+    image_name = models.CharField(max_length=50)
+    image_description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.image_name
+    class Meta:
+        ordering = ['image_name']
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=50)
