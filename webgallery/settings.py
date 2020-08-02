d648177c724e55b7d82e2ed92c353682cee79830
Fred Ojure <fredojure@hotmail.com>
2020-08-02 13:19:27 +0300

    Added a css file and connected to base template

--- webgallery/settings.py
+++ webgallery/settings.py
@@ -20,7 +20,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'gallery.apps.GalleryConfig',
+    'gallery',
     'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
