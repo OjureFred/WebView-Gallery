09203eef0fd26fa05cae2ba6ac899b3817ca01f5
Fred Ojure <fredojure@hotmail.com>
2020-08-02 19:16:47 +0300

    Defined media root for application

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -7,3 +9,5 @@ urlpatterns = [
     url('^location/$', views.picture_location, name ='pictureLocation'),
     url(r'^search/', views.search_results, name='search_results')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
